906db0fc07ae9874c9f3dee0401fb4586ce67e97
Felipe Oliveira <felipeoliveiradeveloper@gmail.com>
2020-08-19 20:45:17 -0300

    teste4

---
+++ js/slider.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+  $("#series").lightSlider({
+    item: 4,
+    autoWidth: false,
+    slideMove: 1, // slidemove will be 1 if loop is true
+    slideMargin: 24,
+    enableDrag: true,
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: {
+          item: 2,
+          slideMove: 1,
+          slideMargin: 50,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          item: 1,
+          slideMove: 1,
+          slideMargin: 100,
+        },
+      },
+    ],
+  });
+  $("#movies").lightSlider({
+    item: 4,
+    autoWidth: false,
+
+    slideMove: 1, // slidemove will be 1 if loop is true
+    slideMargin: 24,
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: {
+          item: 2,
+          slideMove: 1,
+          slideMargin: 50,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          item: 1,
+          slideMove: 1,
+          slideMargin: 50,
+        },
+      },
+    ],
+  });
+});
+
+Modernizr.on("webp", function (result) {
+  if (result) {
+    $(".img-webp").each(function () {
+      this.src = $(this).data("webp");
+    });
+  } else {
+    $(".img-webp").each(function () {
+      this.src = $(this).data("original");
+    });
+  }
+});
