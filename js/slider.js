0763c1a8f0cbc7b9356709d5c6c1f248afde34ad
Higor Mattos <53987364+Higormattosh@users.noreply.github.com>
2020-08-19 14:49:31 -0300

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -50,3 +50,15 @@ $(document).ready(function () {
     ],
   });
 });
+
+Modernizr.on('webp', function (result) {
+    if (result) {
+        $('.img-webp').each(function () {
+            this.src = $(this).data('webp');
+        });
+    } else {
+        $('.img-webp').each(function () {
+            this.src = $(this).data('original');
+        });
+    }
+});
