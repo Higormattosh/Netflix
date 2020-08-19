8bd5e192613d18315aaf5d8972c7fa347649700c
Felipe Oliveira <felipeoliveiradeveloper@gmail.com>
2020-08-19 02:47:10 -0300

    initial

---
+++ js/slider.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+  $("#lightSlider").lightSlider({
+    item: 4,
+    autoWidth: false,
+    slideMove: 1, // slidemove will be 1 if loop is true
+    slideMargin: 24,
+    enableDrag: true,
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: {
+          item: 2,
+          slideMove: 1,
+          slideMargin: 50,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          item: 1,
+          slideMove: 1,
+          slideMargin: 100,
+        },
+      },
+    ],
+  });
+  $("#lightSlider2").lightSlider({
+    item: 4,
+    autoWidth: false,
+
+    slideMove: 1, // slidemove will be 1 if loop is true
+    slideMargin: 24,
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: {
+          item: 2,
+          slideMove: 1,
+          slideMargin: 50,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          item: 1,
+          slideMove: 1,
+          slideMargin: 50,
+        },
+      },
+    ],
+  });
+});
