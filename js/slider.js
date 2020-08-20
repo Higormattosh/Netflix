870d853d90266edf433a7944c5c7fad53167dbaf
Felipe Oliveira <felipeoliveiradeveloper@gmail.com>
2020-08-19 21:48:43 -0300

    teste

--- js/slider.js
+++ js/slider.js
@@ -50,15 +50,3 @@ $(document).ready(function () {
     ],
   });
 });
-
-Modernizr.on("webp", function (result) {
-  if (result) {
-    $(".img-webp").each(function () {
-      this.src = $(this).data("webp");
-    });
-  } else {
-    $(".img-webp").each(function () {
-      this.src = $(this).data("original");
-    });
-  }
-});
